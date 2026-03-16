a62a0fa2818f3a1d6fb16da806e1223b4a5fdc93
DESKTOP-T9HI5PU\Ihe box <terranko@gmail.com>
2026-03-16 18:10:02 +0900

    update

--- csharp3/obj/Release/net10.0/csharp3.AssemblyInfo.cs
+++ csharp3/obj/Release/net10.0/csharp3.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp3")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53184eca9c265a61e09d2296615ded73a8e4c0b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp3")]
